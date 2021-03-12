31f6f2273f11f6fa3c6348574d9cf2eed3a5d55c
Vezon Quentin <quentin.vezon@etu.umontpellier.fr>
2021-03-12 15:37:53 +0100

    Ex2 debut Q1

--- src/main/java/fr/umontpellier/iut/partie1/Couple.java
+++ src/main/java/fr/umontpellier/iut/partie1/Couple.java
@@ -17,7 +17,14 @@ public class Couple {
     }
 
     public ArrayList<Taquin> getListeDeMouvements() {
-        throw new RuntimeException("Méthode non implémentée ! Effacez cette ligne et écrivez le code nécessaire");
+
+        ArrayList<Taquin> List = new ArrayList<>();
+        Couple temp = this;
+        while(temp.predecesseur != null){
+            List.add(0,temp.taquin);
+        }
+       return(List);
+
     }
 
     public Taquin getTaquin() {
