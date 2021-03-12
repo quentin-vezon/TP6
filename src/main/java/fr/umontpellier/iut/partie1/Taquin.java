fde3c5c2d7e085100292ee6196bc68dedcc09f2a
Vezon Quentin <quentin.vezon@etu.umontpellier.fr>
2021-03-12 11:24:36 +0100

    Q1/Q2/Q3

--- src/main/java/fr/umontpellier/iut/partie1/Taquin.java
+++ src/main/java/fr/umontpellier/iut/partie1/Taquin.java
@@ -1,6 +1,7 @@
 package fr.umontpellier.iut.partie1;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Taquin {
     private int[][] tableau;
@@ -10,7 +11,21 @@ public class Taquin {
     }
 
     public boolean estGagnant() {
-        throw new RuntimeException("Méthode non implémentée ! Effacez cette ligne et écrivez le code nécessaire");
+        int n = 0;
+        for(int i =0; i < tableau.length; i++){
+            for(int j = 0; j < tableau[i].length; j++){
+                int res = tableau[i][j];
+                if(res == 0 && i == tableau.length-1 && j == tableau[i].length-1) {
+                    return true;
+                    }
+                else if(res != n) {
+                }
+                else {
+                    n += 1;
+                }
+            }
+        }
+        return true;
     }
 
     public ArrayList<Taquin> genererFils() {
@@ -22,4 +37,47 @@ public class Taquin {
         throw new RuntimeException("Méthode non implémentée ! Effacez cette ligne et écrivez le code nécessaire");
     }
 
+
+    @Override
+    public String toString() {
+        String res = "+-----------+\n";
+        String sres = res;
+        for( int n = 0; n < tableau.length; n++){
+            for (int p = 0; p < tableau[n].length; p++){
+                int tab = tableau[n][p];
+                sres += p == 0 ? "| " + tab :(p+1 == tableau[n].length ? " " + tab + " |\n" : " ");
+            }
+        }
+        sres += res;
+        return sres;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Taquin taquin = (Taquin) o;
+        if (tableau.length != taquin.tableau.length) {
+            return false;
+        }
+        for (int i = 0; i < tableau.length; i++) {
+            if (tableau[i].length != taquin.tableau[i].length) {
+                return false;
+            }
+
+            for (int j = 0; j < tableau[i].length; j++) {
+                if (tableau[i][j] != taquin.tableau[i][j]) {
+                    return false;
+                }
+            }
+
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(tableau);
+    }
 }
